9ecbbf8cb21418802040d352a9ffdc3e6dbb527f
Wallace_Lee <gn01675522@gmail.com>
2024-12-29 21:56:07 +0800

    Modified Webpack setting, and add README

---
+++ README.md
@@ -0,0 +1,29 @@
+# 使用方式
+
+1.  請先於您本地安裝 `Node.js` 及 `NPM`
+2.  開啟 terminal 並使用 `git clone https://github.com/gn01675522/the-Ace-clothing.git`
+3.  使用 `npm i` 安裝 package 套件
+4.  使用 `npm start` 開啟專案 (development mode)
+
+## 專案說明
+
+1. 專案名稱： the Ace clothing
+2. 專案類型： 電商網站
+
+## 專案使用技術
+
+1.  主要技術：TypeScript、HTML、CSS
+2.  主要框架/函式庫：React.js
+3.  預處理器：SASS(SCSS)
+4.  狀態管理：Redux-toolkit
+5.  API 交互：Axios
+6.  環境變數：dot-env
+7.  狀態監控：Redux logger
+8.  額外套件：React hook form
+9.  打包工具：Webpack
+
+## 專案使用資料來源
+
+1.  API：六角學院
+2.  圖片：unsplash
+3.  icons：Flaticon
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,56 @@
 const webpack = require("webpack");
 const { resolve, join } = require("path");
-const dotenv = require("dotenv");
-
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const env = dotenv.config().parsed;
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
 
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
-
-module.exports = {
+const config = {
   entry: "./src/index.tsx",
   output: {
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
     path: resolve(__dirname, "build"),
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-              "@babel/preset-typescript",
-            ],
+        oneOf: [
+          {
+            test: /\.(js|jsx|ts|tsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  "@babel/preset-env",
+                  ["@babel/preset-react", { runtime: "automatic" }],
+                  "@babel/preset-typescript",
+                ],
+              },
+            },
           },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|bmp|ico)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[hash][ext][query]",
-        },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+          },
+          {
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+          },
+          {
+            test: /\.(png|jpe?g|gif|bmp|ico)$/i,
+            type: "asset/resource",
+            generator: {
+              filename: "images/[hash][ext][query]",
+            },
+          },
+        ],
       },
     ],
   },
@@ -71,6 +70,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
     }),
     new webpack.DefinePlugin({
       "process.env.APP_API_URL": JSON.stringify(process.env.APP_API_URL || ""),
@@ -78,7 +81,27 @@ module.exports = {
         process.env.APP_API_PATH || ""
       ),
     }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: mode === "production" ? "static" : "disabled",
+      openAnalyzer: false,
+      reportFilename: "bundle-report.html",
+    }),
   ],
-  mode: "development",
-  devtool: process.env.NODE_ENV === "development" ? "source-map" : false,
+  optimization: {
+    // minimize: mode === "production",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  mode: mode,
+  devtool: mode === "development" ? "source-map" : false,
 };
+
+module.exports = config;
