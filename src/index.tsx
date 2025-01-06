7ff342c549ea51c50d4dfc8ba712a21c5996d4e0
Wallace_Lee <gn01675522@gmail.com>
2025-01-06 22:04:43 +0800

    Optimized Webpack setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import axios from "axios";
@@ -19,10 +20,10 @@ const root = ReactDOM.createRoot(element);
 
 root.render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
