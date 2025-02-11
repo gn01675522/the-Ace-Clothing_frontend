1ed63fb6fe82391786f3b83aa18148c9d5609d72
Wallace_Lee <gn01675522@gmail.com>
2025-02-11 15:04:37 +0800

    Add DeleteModal tests

---
+++ src/components/DeleteModal/__tests__/deleteModal.test.tsx
@@ -0,0 +1,46 @@
+import { screen, fireEvent } from "@testing-library/react";
+import { renderWithProviders } from "../../../utils/test.utils";
+
+import DeleteModal from "../DeleteModal.component";
+
+import { DELETE_MODAL_TYPE } from "../DeleteModal.component";
+
+describe("DeleteModal test suite.", () => {
+  test("Clicking on the 'ｘ', '取消', '確認刪除' buttons triggers the closeAction callback.", async () => {
+    const onClick = jest.fn();
+    renderWithProviders(
+      <DeleteModal
+        dataType={DELETE_MODAL_TYPE.adminCoupon}
+        id="test"
+        title="test title"
+        closeAction={onClick}
+      />
+    );
+
+    const closeButton = screen.getByText(/取消/i);
+    const closeXButton = screen.getByLabelText(/Close/i);
+    const confirmDeleteButton = screen.getByText(/確認刪除/i);
+
+    await fireEvent.click(closeButton);
+    await fireEvent.click(closeXButton);
+    await fireEvent.click(confirmDeleteButton);
+
+    expect(onClick).toHaveBeenCalledTimes(3);
+  });
+  test("Should display title when passed title prop.", () => {
+    const title = "test title";
+
+    renderWithProviders(
+      <DeleteModal
+        dataType={DELETE_MODAL_TYPE.adminCoupon}
+        id="test"
+        title={title}
+        closeAction={() => {}}
+      />
+    );
+
+    const titleElement = screen.getByText(`確定刪除 ${title}？`);
+
+    expect(titleElement).toBeInTheDocument();
+  });
+});
