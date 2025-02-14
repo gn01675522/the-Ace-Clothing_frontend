4e436a4f5dc772f6c3e778c064102bb0690f88cd
Wallace_Lee <gn01675522@gmail.com>
2025-02-14 23:05:39 +0800

    Add Pagination tests

--- src/components/OrderModal/__tests__/orderModal.test.tsx
+++ src/components/OrderModal/__tests__/orderModal.test.tsx
@@ -5,56 +5,59 @@ import OrderModal from "../OrderModal.component";
 
 import type { Order } from "store/adminOrder/adminOrders.type";
 
-const orderData = {
-  title: "test order",
-  create_at: 1736553600000,
-  id: "test id",
-  is_paid: true,
-  message: "",
-  products: [
-    {
-      id: "test product",
-      product_id: "test product id",
-      qty: 2,
-      coupon: {
-        id: "test coupon id",
-        title: "test coupon title",
-        is_enabled: 1,
-        percent: 80,
-        due_date: 1736553600000,
-        code: "test coupon code",
-        num: 2,
-      },
-      final_total: 1600,
-      product: {
-        id: "test product id",
-        category: "shoe",
-        content: "test content",
-        description: "test description",
-        imageUrl: "https://test.com",
-        imagesUrl: ["https://test1.com", "https://test2.com"],
-        is_enabled: 1,
-        num: 3,
-        origin_price: 2000,
-        price: 1800,
-        title: "test title",
-        unit: "unit",
-      },
-      total: 2000,
-    },
-  ],
-  status: 1,
-  total: 1600,
-  user: {
-    address: "test address",
-    email: "test@test.com",
-    name: "test",
-    tel: "1112223344",
-  },
-} as Order;
-const onClick = jest.fn();
-
 describe("OrderModal test suite.", () => {
+  const orderData = {
+    title: "test order",
+    create_at: 1736553600000,
+    id: "test id",
+    is_paid: true,
+    message: "",
+    products: [
+      {
+        id: "test product",
+        product_id: "test product id",
+        qty: 2,
+        coupon: {
+          id: "test coupon id",
+          title: "test coupon title",
+          is_enabled: 1,
+          percent: 80,
+          due_date: 1736553600000,
+          code: "test coupon code",
+          num: 2,
+        },
+        final_total: 1600,
+        product: {
+          id: "test product id",
+          category: "shoe",
+          content: "test content",
+          description: "test description",
+          imageUrl: "https://test.com",
+          imagesUrl: ["https://test1.com", "https://test2.com"],
+          is_enabled: 1,
+          num: 3,
+          origin_price: 2000,
+          price: 1800,
+          title: "test title",
+          unit: "unit",
+        },
+        total: 2000,
+      },
+    ],
+    status: 1,
+    total: 1600,
+    user: {
+      address: "test address",
+      email: "test@test.com",
+      name: "test",
+      tel: "1112223344",
+    },
+  } as Order;
+
+  const onClick = jest.fn();
+
+  beforeEach(() => onClick.mockClear());
+
   test("Should render order data correctly.", () => {
     renderWithProviders(
       <OrderModal targetData={orderData} closeAction={onClick} />
