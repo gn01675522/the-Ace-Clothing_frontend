9c4b241d319cbc98b9d3dff52a0cf92e82fba20c
Wallace_Lee <gn01675522@gmail.com>
2025-02-15 22:43:16 +0800

    Add SummaryCard test

---
+++ src/components/SummaryCard/__tests__/summaryCard.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+
+import SummaryCard from "../SummaryCard.component";
+
+import { formatNumberWithCommas } from "../../../utils/common.utils";
+
+describe("SummaryCard test suite.", () => {
+  const mockData = {
+    total: 2000,
+    userData: {
+      user: {
+        name: "test",
+        tel: "0000000000",
+        address: "test address",
+        email: "test@test.com",
+      },
+    },
+  };
+  test("Should render correctly when every props has data.", () => {
+    render(<SummaryCard total={mockData.total} userData={mockData.userData} />);
+
+    const totalPriceElement = screen.getAllByText(
+      `NT$ ${formatNumberWithCommas(Math.round(mockData.total))}`
+    );
+    const nameElement = screen.getByText(mockData.userData.user.name);
+    const telElement = screen.getByText(mockData.userData.user.tel);
+    const addressElement = screen.getByText(mockData.userData.user.address);
+    const emailElement = screen.getByText(mockData.userData.user.email);
+
+    expect(totalPriceElement).toHaveLength(2);
+    expect(nameElement).toBeInTheDocument();
+    expect(telElement).toBeInTheDocument();
+    expect(addressElement).toBeInTheDocument();
+    expect(emailElement).toBeInTheDocument();
+  });
+});
