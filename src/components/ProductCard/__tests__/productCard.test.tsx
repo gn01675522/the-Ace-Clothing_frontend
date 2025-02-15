9ba46cd76a5158856b870930fbe4f93e9c69fd71
Wallace_Lee <gn01675522@gmail.com>
2025-02-15 22:26:45 +0800

    Add ProductModal tests

--- src/components/ProductCard/__tests__/productCard.test.tsx
+++ src/components/ProductCard/__tests__/productCard.test.tsx
@@ -1,4 +1,4 @@
-import { screen, fireEvent } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../../utils/test.utils";
 
 import ProductCard from "../ProductCard.component";
