82bee3df799466335165ddb5de58d867adb5a1d8
Wallace_Lee <gn01675522@gmail.com>
2025-09-07 22:32:02 +0800

    chore: seperate scss code in Success page

--- src/features/product/modules/recommend-list/RecommendList.module.tsx
+++ src/features/product/modules/recommend-list/RecommendList.module.tsx
@@ -4,13 +4,15 @@ import { ProductCard, selectRecommendProducts } from "../../../product/index";
 
 import type { FC } from "react";
 
+import "./RecommendList.styles.scss";
+
 export const RecommendList: FC = () => {
   const recommendProducts = useAppSelector(selectRecommendProducts);
 
   return (
-    <div className="success__recommend">
-      <h2 className="success__recommend-title">您或許還想買</h2>
-      <div className="success__recommend-card">
+    <div className="recommend-list">
+      <h2 className="recommend-list__title">您或許還想買</h2>
+      <div className="recommend-list__card">
         {recommendProducts.map((product) => {
           return <ProductCard product={product} key={product.id} />;
         })}
